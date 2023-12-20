60b44a4598f4695e6abc98c00afc0f0736786f65
gdsr-abhishek <gdsr.abhishek@gmail.com>
2023-12-20 23:42:01 +0530

    Sending response as JSONArray to the Client and changed the command

--- Service/processService.js
+++ Service/processService.js
@@ -1,8 +1,8 @@
 import childProcess from "child_process"; 
 export class processService{
  static getAllService(callback){
-    childProcess.exec('ps -l ',(err,stdout,stderr)=>{
-            console.info(`stdout : ${typeof stdout}`);
+    childProcess.exec('ps -A u',(err,stdout,stderr)=>{
+            console.info(`stdout : ${stdout}`);
             callback(stdout);
     })
  }
--- index.js
+++ index.js
@@ -5,21 +5,26 @@ const port = 8080;
 const app = express();
 
 app.get('/ps/all',function(req,res){
-  let result=[]    
+  let result=[];
   processService.getAllService((stdout)=>{
          
         result = stdout.split('\n');
+        let columns =result[0].split(" ").filter((v)=>v!=="");
+        result.splice(0,1)
         for(let i =0 ; i< result.length; i++){
           let temp = result[i].split(" ");
-          result[i]= [];
+          result[i]={};
+          let j=0
           temp.forEach(element => {
           if(element !=""){
-           result[i].push(element);
+           result[i][columns[j]]= element;
+           j++; 
           }          
           });
             
          
         }   
+        result.pop();
         res.status(200).send(result);  
       });
 
